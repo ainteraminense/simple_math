f7b370ba97280c8ce05ec8078430c479ccfcd1e2
Armando Interaminense <ar.interaminense@gmail.com>
2022-11-15 11:50:56 -0500

    added docstring for the script and the main function

--- simple_math.py
+++ simple_math.py
@@ -1,8 +1,29 @@
-first_question = "What is the first number? "
+"""Simple Math
+
+This script get two inputs of the user and do mathematics operations. For each one it will return the result and print to the user.
+
+This file can also be imported as a module and contains the following functions:
+
+    * ask_user_input - return the input entered as string
+    * convert_to_number - return the string input as int
+    * sum - return the sum of two numbers
+    * minus - return the subtraction of two numbers
+    * multiply - return the multiplication of two numbers
+    * divide - return the division of two numbers
+"""
 
 
-def askUserInput(question: str):# holding alt and left click add multiple cursor functionality
-    return input(question)# holding alt and left click add multiple cursor functionality
+def ask_user_input(question: str) -> str:
+    # this function receive a string with a question
+    # it uses that question to get a user input
+    # return the user input
+    return input(question)
 
-# this line should be comented out
-# select the whole area and press ctrl /
+
+def main():
+    question = "What is the first number? "
+    user_input_string = ask_user_input(question)
+
+
+if(__name__ == "__main__"):
+    main()
