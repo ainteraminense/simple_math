92e61793ecdb2d589a1ea54c85079c7b6a80ad6f
Armando Interaminense <ar.interaminense@gmail.com>
2022-11-15 16:42:12 -0500

    added convert to number function

--- simple_math.py
+++ simple_math.py
@@ -14,15 +14,23 @@ This file can also be imported as a module and contains the following functions:
 
 
 def ask_user_input(question: str) -> str:
-    # this function receive a string with a question
-    # it uses that question to get a user input
-    # return the user input
     return input(question)
+
+
+def convert_to_number(number_string):
+    try:
+        return int(number_string)
+    except ValueError:
+        return print("Error. Please enter a valid digit")
 
 
 def main():
     question = "What is the first number? "
-    user_input_string = ask_user_input(question)
+    user_input1_string = ask_user_input(question)
+    user_input1_number = convert_to_number(user_input1_string)
+    question = "What is the second number? "
+    user_input2_string = ask_user_input(question)
+
 
 
 # if(__name__ == "__main__"):
