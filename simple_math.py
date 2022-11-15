6486605b6c9f1419b60d18078878da14dfe1b2db
Armando Interaminense <ar.interaminense@gmail.com>
2022-11-15 11:06:51 -0500

    wrote some tips for commenting

--- simple_math.py
+++ simple_math.py
@@ -1,7 +1,8 @@
 first_question = "What is the first number? "
 
-def askUserInput(question: str):
-    return input(question)
 
-x = askUserInput(first_question)
-print(x)
+def askUserInput(question: str):# holding alt and left click add multiple cursor functionality
+    return input(question)# holding alt and left click add multiple cursor functionality
+
+# this line should be comented out
+# select the whole area and press ctrl /
